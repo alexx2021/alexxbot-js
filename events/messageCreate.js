94ef7b34c9a8cd20b4c78c0bd35180951bb0874b
alexx2021 <78677978+alexx2021@users.noreply.github.com>
2021-08-30 16:44:20 -0700

    add eval cmd

---
+++ events/messageCreate.js
@@ -0,0 +1,30 @@
+module.exports = {
+	name: 'messageCreate',
+	execute(message) {
+        function clean(text) {
+            if (typeof(text) === "string")
+              return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+            else
+                return text;
+          }
+
+
+        const args = message.content.split(" ").slice(1);
+  
+        if (message.content.startsWith("!eval")) {
+          if (message.author.id !== "247932598599417866") return;
+          try {
+            const code = args.join(" ");
+            let evaled = eval(code);
+      
+            if (typeof evaled !== "string")
+              evaled = require("util").inspect(evaled);
+      
+            message.channel.send((evaled), {code:"xl"});
+          } catch (err) {
+            message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+          }
+        }
+	},
+};
+
