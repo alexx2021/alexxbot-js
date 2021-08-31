8dd4d4bf73b79d442ead498659d0cacb444d4d6f
alexx2021 <78677978+alexx2021@users.noreply.github.com>
2021-08-30 21:07:34 -0700

    update formatting

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,6 @@
 module.exports = {
-	name: 'interactionCreate',
-	execute(interaction) {
-		console.log(`${interaction.user.tag} (${interaction.user.id}) in #${interaction.channel.name} (${interaction.guild.name}) triggered an interaction (${interaction.commandName}).`);
-	},
+    name: 'interactionCreate',
+    execute(interaction) {
+        console.log(`${interaction.user.tag} (${interaction.user.id}) in #${interaction.channel.name} (${interaction.guild.name}) triggered an interaction (${interaction.commandName}).`);
+    },
 };
